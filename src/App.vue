19289c580d1088df0c69171292181a0f822ed5f7
Shaki Ekanayake <shaki.ekanayake@gmail.com>
2021-01-29 03:17:26 +0530

    component rename to FullPage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FullPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullPage from './components/FullPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FullPage
   }
 }
 </script>
