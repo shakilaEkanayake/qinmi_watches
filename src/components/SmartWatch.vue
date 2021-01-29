55d2d5eb6d2bb39d60b666329cbf72ec36f56bd4
Shaki Ekanayake <shaki.ekanayake@gmail.com>
2021-01-29 14:34:14 +0530

    clean up

--- src/components/SmartWatch.vue
+++ src/components/SmartWatch.vue
@@ -7,6 +7,7 @@
 <script>
 import * as Three from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 export default {
     name: "SmartWatch",
@@ -19,58 +20,45 @@ export default {
             model: null,
             clock: null,
             raycaster: null,
-            stacy_txt: null,
         };
     },
     methods: {
         init() {
-            // const MODEL_PATH = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/stacy_lightweight.glb";
             const MODEL_PATH = "assets/models/apple-watch-v2/scene.gltf";
-            // const TEXTURE_PATH = "assets/models/apple-watch/textures/material_baseColor.jpeg";
-            // const TEXTURE_PATH = "assets/models/apple-watch-v3/textures/";
-            // const TEXTURE_PATH = "./apple-watch-v2/textures";
+            // const TEXTURE_PATH = "assets/models/apple-watch-v2/textures";
 
             let container = document.getElementById("container");
             let canvas = document.getElementById("canvas");
 
-            this.clock = new Three.Clock(); // Used for anims, which run to a clock instead of frame rate
-            this.currentlyAnimating = false; // Used to check whether characters neck is being used in another anim
-            this.raycaster = new Three.Raycaster(); // Used to detect the click on our character
+            this.clock = new Three.Clock();
+            this.currentlyAnimating = false;
+            this.raycaster = new Three.Raycaster();
 
             this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10);
             this.camera.position.z = 1;
 
             this.scene = new Three.Scene();
 
-            // this.stacy_txt = new Three.TextureLoader().load(TEXTURE_PATH);
-            // this.stacy_txt.flipY = false; // we flip the texture so that its the right way up
-            // const stacy_mtl = new Three.MeshPhongMaterial({
-            //     map: this.stacy_txt,
-            //     color: 0xffffff,
-            //     skinning: true,
-            // });
-
             var loader = new GLTFLoader();
+
+            // const dracoLoader = new DRACOLoader();
+            // dracoLoader.setDecoderPath("/examples/js/libs/draco/");
+            // loader.setDRACOLoader(dracoLoader);
+
             // loader.setResourcePath(TEXTURE_PATH);
             loader.load(
                 MODEL_PATH,
                 (gltf) => {
                     this.model = gltf.scene;
-                    // let fileANimations = gltf.animations;
 
                     this.model.traverse((o) => {
                         if (o.isMesh) {
                             o.castShadow = true;
                             o.receiveShadow = true;
-                            // o.material = this.stacy_mtl; // Add this line
                         }
                     });
 
                     this.model.scale.set(0.4, 0.4, 0.4);
-                    // this.model.position.y = -1;
-
-                    // console.log(this.model);
-                    // gltf.animations;
 
                     this.scene.add(this.model);
                 },
@@ -82,26 +70,6 @@ export default {
                 }
             );
 
-            // this.model = model;
-            // let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-            // let material = new Three.MeshNormalMaterial();
-
-            // this.mesh = new Three.Mesh(geometry, material);
-            // this.scene.add(this.mesh);
-
-            // Floor
-            let floorGeometry = new Three.PlaneGeometry(5000, 5000, 1, 1);
-            let floorMaterial = new Three.MeshPhongMaterial({
-                color: 0xeeeeee,
-                shininess: 0,
-            });
-
-            let floor = new Three.Mesh(floorGeometry, floorMaterial);
-            floor.rotation.x = -0.5 * Math.PI; // This is 90 degrees by the way
-            floor.receiveShadow = true;
-            floor.position.y = -11;
-            this.scene.add(floor);
-
             this.renderer = new Three.WebGLRenderer({ canvas, antialias: true, alpha: true });
             this.renderer.shadowMap.enabled = true;
             this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -111,17 +79,11 @@ export default {
         animate() {
             requestAnimationFrame(this.animate);
 
-            // this.model.rotation.x += 0.01;
-            // console.log(this.model);
             if (this.model) {
                 this.model.position.y = 0;
-                // this.model.rotation.x += 0.01;
                 this.model.rotation.y += 0.02;
             }
 
-            // this.model.position.y = 0;
-            // this.mesh.rotation.x += 0.01;
-            // this.mesh.rotation.y += 0.02;
             this.renderer.render(this.scene, this.camera);
         },
     },
