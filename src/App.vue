22d7816db8eef1bc1824b80cf6ad5ca3b4dee850
Shaki Ekanayake <shaki.ekanayake@gmail.com>
2021-01-29 09:34:13 +0530

    made three js working

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="app">
-    <FullPage msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <FullPage msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
-import FullPage from './components/FullPage.vue'
+import FullPage from "./components/FullPage.vue";
 
 export default {
-  name: 'App',
-  components: {
-    FullPage
-  }
-}
+    name: "App",
+    components: {
+        FullPage,
+    },
+};
 </script>
 
-<style>
-</style>
+<style></style>
